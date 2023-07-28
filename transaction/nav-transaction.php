d53a9ed6b64b228e50381e99637c296ffb4ab326
Joseph Then <josephthen3320@outlook.com>
2023-07-28 14:53:16 +0700

    refactor: change <button> to <a> for navbar buttons

--- transaction/nav-transaction.php
+++ transaction/nav-transaction.php
@@ -1,27 +1,19 @@
 <div id='' class='w3-bar w3-black w3-border-bottom w3-hide-small'>
-    <?php
-        $btnPolaMarker = "<button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/pola-marker\")' >Pola Marker</button>";
-        echo ($_SESSION['user_role'] != 4) ? $btnPolaMarker : '';
-    ?>
+    <?php if ($_SESSION['user_role'] != 4) : ?>
+        <a class='w3-bar-item w3-button' href='/transaction/pola-marker'>Pola Marker</a>
+    <?php endif; ?>
 
-    <button class='w3-bar-item w3-button' onclick='openURL("/transaction/cutting")' >Cutting</button>
-    
-    <?php
-        $btnOthers = "
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/embro\")' >Embro</button>
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/print-sablon\")' >Print/Sablon</button>
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/qc-embro\")' >QC Embro</button>
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/sewing\")' >Sewing</button>
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/washing\")' >Washing</button>
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/finishing\")' >Finishing</button>
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/qc-final\")' >QC Final</button>
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/transaction/perbaikan\")' >Perbaikan</button>
-            
-            <button class='w3-bar-item w3-button' onclick='openURL(\"/warehouse\")' >Gudang</button>
-            ";
+    <a class='w3-bar-item w3-button' href='/transaction/cutting'>Cutting</a>
 
-        echo ($_SESSION['user_role'] != 4) ? $btnOthers : '';
-
-
-    ?>
+    <?php if ($_SESSION['user_role'] != 4) : ?>
+        <a class='w3-bar-item w3-button' href='/transaction/embro'>Embro</a>
+        <a class='w3-bar-item w3-button' href='/transaction/print-sablon'>Print/Sablon</a>
+        <a class='w3-bar-item w3-button' href='/transaction/qc-embro'>QC Embro</a>
+        <a class='w3-bar-item w3-button' href='/transaction/sewing'>Sewing</a>
+        <a class='w3-bar-item w3-button' href='/transaction/washing'>Washing</a>
+        <a class='w3-bar-item w3-button' href='/transaction/finishing'>Finishing</a>
+        <a class='w3-bar-item w3-button' href='/transaction/qc-final'>QC Final</a>
+        <a class='w3-bar-item w3-button' href='/transaction/perbaikan'>Perbaikan</a>
+        <a class='w3-bar-item w3-button' href='/warehouse'>Gudang</a>
+    <?php endif; ?>
 </div>
